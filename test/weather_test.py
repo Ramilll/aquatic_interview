9610ad9b466caba4726e79f6b4e35c71f00538bf
Ramil Nazmeev <ramil@nazmeev.ru>
2022-06-06 21:20:55 +0300

    added tests

--- test/weather_test.py
+++ test/weather_test.py
@@ -1,9 +1,24 @@
 from interview import weather
+from utils.utils import read_file
 import io
 
-
-def test_replace_me():
-    reader = io.StringIO("Line One\nLine Two\n")
+def baseline_test(test_number):
+    reader = io.StringIO(read_file("test/{}/input.txt".format(test_number)))
     writer = io.StringIO()
     weather.process_csv(reader, writer)
-    assert writer.getvalue() == "Saw 2 lines\n"
+    assert writer.getvalue() == read_file("test/{}/output.txt".format(test_number)), "incorrect output on test number {}".format(test_number)
+
+def test_zero():
+    baseline_test(0)
+
+def test_one():
+    baseline_test(1)
+
+def test_two():
+    baseline_test(2)
+
+def test_three():
+    baseline_test(3)
+
+def test_four():
+    baseline_test(4)
